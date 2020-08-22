71283978aa48c93d965abd1455ec486ca090d049
Ben Haubrich <bjh885@mail.usask.ca>
2020-08-22 14:46:55 -0600

    re-order if statements to check root first

--- change_session_manager.bash
+++ change_session_manager.bash
@@ -6,13 +6,13 @@
 #Date    : August 27th, 2020                                                  #
 ###############################################################################
 
-if [ $# -le 0 ]; then
+if [ `whoami` != "root" ]; then
+  echo "Please re-run as root (i.e sudo change_session_manager)"
+  exit 1
+elif [ $# -le 0 ]; then
   echo "Usage: change_session_manager <path-to-session-manager>"
   echo "Available Managers:"
   update-alternatives --list x-session-manager
-  exit 1
-elif [ `whoami` != "root" ]; then
-  echo "Please re-run as root (i.e sudo change_session_manager)"
   exit 1
 fi
 
